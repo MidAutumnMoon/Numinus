2c66f1a07fb28b31a7b329e1f3b41d3ebef74096
MidAutumnMoon <me@418.im>
2022-04-29 20:44:34 +0800

    update: rewrite EditHere in lua

---
+++ plugin/EditHere.lua
@@ -0,0 +1,38 @@
+--
+-- Edit a file related to the current opened file.
+-- Kind like what Emacs does.
+--
+
+EditHere = {}
+
+
+-- dir_of_this_file :: string
+--
+-- Get the directory of current file.
+--
+EditHere.dir_of_this_file = function()
+  return vim.fn.expand( '%:p:h' )
+end
+
+
+-- related_to_cwd :: string
+--
+-- Return the part of current file's
+-- path which is related to CWD.
+--
+EditHere.related_to_cwd = function()
+  local cwd = vim.fn.getcwd()
+  local here = EditHere.dir_of_this_file()
+
+  return vim.fn.substitute(
+      here, cwd, '.', ''
+    )
+end
+
+
+--
+-- Set the keymap.
+--
+vim.cmd [[
+  nnoremap ,e :e <C-r>=luaeval( 'EditHere.related_to_cwd()' )<CR>
+]]
--- plugin/EditHere.vim
+++
@@ -1,6 +0,0 @@
-"
-" Edit a file related to the current opened file.
-" Kind like what Emacs does.
-"
-
-nnoremap ,e :e <C-r>=expand('%:p:h')<CR>
