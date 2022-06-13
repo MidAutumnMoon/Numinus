aa20af4dfd32defa7deb2ba25e471f0a9f38179f
MidAutumnMoon <me@418.im>
2022-06-13 13:55:13 +0800

    packer: fix conflicts between quick-scope and hop

--- lua/configs/quick-scope.lua
+++ lua/configs/quick-scope.lua
@@ -1,3 +1,4 @@
-vim.g.qs_highlight_on_keys = {
-  'f', 'F'
-}
+vim.g.qs_highlight_on_keys = { }
+
+vim.keymap.set( 'n', 'f', '<Plug>(QuickScopef)', { silent = true } )
+vim.keymap.set( 'n', 'F', '<Plug>(QuickScopeF)', { silent = true } )
--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -3,170 +3,169 @@ require 'packer/bootstrap'
 
 require 'packer'.startup( function()
 
-  -- Impatient!
-
-  use { 'lewis6991/impatient.nvim' }
-
-
-
-  -- Editing
-
-  use 'tpope/vim-repeat'
-  use 'tommcdo/vim-exchange'
-  use 'wellle/targets.vim'
-
-  use { 'tpope/vim-commentary',
-        keys = { 'gc', 'gcc' } }
-
-  use { 'junegunn/vim-after-object',
-        keys = 'a',
-        config = [[ require 'configs/after-object' ]] }
-
-  use { 'junegunn/vim-easy-align',
-        keys = { 'ga' },
-        config = [[ require 'configs/easy-align' ]] }
-
-  use { 'machakann/vim-sandwich',
-        event = { 'CursorMoved', 'CursorHold' },
-        config = [[ require 'configs/sandwich' ]] }
-
-  use { 'windwp/nvim-autopairs',
-        event = { 'InsertEnter' },
-        config = [[ require 'configs/autopairs' ]] }
+    -- Impatient!
+    use { 'lewis6991/impatient.nvim' }
 
 
 
-  -- QoLI
+    -- Editing
 
-  use { 'lukas-reineke/indent-blankline.nvim',
-        event = { 'CursorHold', 'CursorMoved' },
-        config = [[ require 'configs/indent-blankline' ]] }
+    use 'tpope/vim-repeat'
+    use 'tommcdo/vim-exchange'
+    use 'wellle/targets.vim'
 
-  use { 'Pocco81/AutoSave.nvim',
-        event = { 'InsertEnter', 'CursorMoved' },
-        config = [[ require 'configs/autosave' ]] }
+    use { 'tpope/vim-commentary',
+          keys = { 'gc', 'gcc' } }
 
-  use { 'airblade/vim-rooter',
-        event = 'VimEnter',
-        config = [[ require 'configs/rooter' ]] }
+    use { 'junegunn/vim-after-object',
+          keys = 'a',
+          config = [[ require 'configs/after-object' ]] }
 
-  use { 'junegunn/vim-slash',
-        keys = { '/', '*' },
-        config = [[ require 'configs/slash' ]] }
+    use { 'junegunn/vim-easy-align',
+          keys = { 'ga' },
+          config = [[ require 'configs/easy-align' ]] }
 
-  use { 'andymass/vim-matchup',
-        event = { 'CursorHold', 'CursorMoved' },
-        config = [[ require 'configs/matchup' ]] }
+    use { 'machakann/vim-sandwich',
+          event = { 'CursorMoved', 'CursorHold' },
+          config = [[ require 'configs/sandwich' ]] }
 
-  use { 'lewis6991/gitsigns.nvim',
-        event = { 'CursorHold', 'CursorMoved' },
-        config = [[ require 'configs/gitsigns' ]] }
-
-  use { 'famiu/bufdelete.nvim',
-        cmd = { 'Bdelete', 'Bwipeout' },
-        keys = '<C-q>',
-        config = [[ require 'configs/bufdelete' ]] }
-
-  use { 'L3MON4D3/LuaSnip',
-        event = 'InsertEnter',
-        module = 'luasnip',
-        config = [[ require 'configs/luasnip' ]] }
-
-  use { 'rafamadriz/friendly-snippets' }
-
-  use { 'gpanders/editorconfig.nvim' }
+    use { 'windwp/nvim-autopairs',
+          event = { 'InsertEnter' },
+          config = [[ require 'configs/autopairs' ]] }
 
 
 
-  -- Navigating
+    -- QoLI
 
-  use { 'unblevable/quick-scope',
-        keys = { 'f', 'F' },
-        setup = [[ require 'configs/quick-scope' ]] }
+    use { 'lukas-reineke/indent-blankline.nvim',
+          event = { 'CursorHold', 'CursorMoved' },
+          config = [[ require 'configs/indent-blankline' ]] }
 
-  use { 'phaazon/hop.nvim',
-        event = { 'CursorMoved', 'CursorHold' },
-        config = [[ require 'configs/hop' ]] }
+    use { 'Pocco81/AutoSave.nvim',
+          event = { 'InsertEnter', 'CursorMoved' },
+          config = [[ require 'configs/autosave' ]] }
 
-  use { 'nvim-telescope/telescope.nvim',
-        event = { 'CursorHold', 'CursorMoved' },
-        keys = '<Leader>f',
-        command = 'Telescope',
-        config = [[ require 'configs/telescope' ]] }
+    use { 'airblade/vim-rooter',
+          event = 'VimEnter',
+          config = [[ require 'configs/rooter' ]] }
 
+    use { 'junegunn/vim-slash',
+          keys = { '/', '*' },
+          config = [[ require 'configs/slash' ]] }
 
+    use { 'andymass/vim-matchup',
+          event = { 'CursorHold', 'CursorMoved' },
+          config = [[ require 'configs/matchup' ]] }
 
-  -- Languages
+    use { 'lewis6991/gitsigns.nvim',
+          event = { 'CursorHold', 'CursorMoved' },
+          config = [[ require 'configs/gitsigns' ]] }
 
-  use { 'eraserhd/parinfer-rust',
-        ft = { 'racket', 'scheme', 'lisp' },
-        run = [[ nix shell nixpkgs#{cargo,gcc} -c cargo build --release ]] }
+    use { 'famiu/bufdelete.nvim',
+          cmd = { 'Bdelete', 'Bwipeout' },
+          keys = '<C-q>',
+          config = [[ require 'configs/bufdelete' ]] }
 
-  use { 'elixir-editors/vim-elixir',
-        ft = { 'elixir' } }
+    use { 'L3MON4D3/LuaSnip',
+          event = 'InsertEnter',
+          module = 'luasnip',
+          config = [[ require 'configs/luasnip' ]] }
 
-  -- use { 'benknoble/vim-racket',
-  --       ft = { 'racket' } }
+    use { 'rafamadriz/friendly-snippets' }
 
-
-
-  -- Cmp
-
-  use { 'hrsh7th/nvim-cmp',
-        event = { 'InsertEnter', 'CursorHold', 'CursorMoved' },
-        config = [[ require 'configs/cmp/cmp' ]] }
-
-  use { 'hrsh7th/cmp-buffer',
-        module = 'cmp_buffer',
-        after = 'nvim-cmp' }
-
-  use { 'hrsh7th/cmp-path',
-        module = 'cmp_path',
-        after = 'nvim-cmp' }
-
-  use { 'hrsh7th/cmp-nvim-lua',
-        after = 'nvim-cmp' }
-
-  use { 'hrsh7th/cmp-nvim-lsp',
-        module = 'cmp_nvim_lsp',
-        after = 'nvim-cmp' }
-
-  use { 'neovim/nvim-lspconfig',
-        module = 'lspconfig',
-        after = 'nvim-cmp' }
-
-  use { 'saadparwaiz1/cmp_luasnip',
-        after = 'nvim-cmp' }
+    use { 'gpanders/editorconfig.nvim' }
 
 
 
-  -- Treesitter
+    -- Navigating
 
-  use { 'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-        config = [[ require 'configs/treesitter' ]] }
+    use { 'unblevable/quick-scope',
+          event = { 'CursorMoved', 'CursorHold' },
+          setup = [[ require 'configs/quick-scope' ]] }
 
-  use { 'p00f/nvim-ts-rainbow' }
+    use { 'phaazon/hop.nvim',
+          event = { 'CursorMoved', 'CursorHold' },
+          config = [[ require 'configs/hop' ]] }
 
-  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-
-
-
-  -- Misc things
-
-  use { 'nvim-lua/plenary.nvim',
-        module = 'plenary' }
-
+    use { 'nvim-telescope/telescope.nvim',
+          event = { 'CursorHold', 'CursorMoved' },
+          keys = '<Leader>f',
+          command = 'Telescope',
+          config = [[ require 'configs/telescope' ]] }
 
 
-  -- Packer itself
 
-  use 'wbthomason/packer.nvim'
+    -- Languages
+
+    use { 'eraserhd/parinfer-rust',
+          ft = { 'racket', 'scheme', 'lisp' },
+          run = [[ nix shell nixpkgs#{cargo,gcc} -c cargo build --release ]] }
+
+    use { 'elixir-editors/vim-elixir',
+          ft = { 'elixir' } }
+
+    -- use { 'benknoble/vim-racket',
+    --       ft = { 'racket' } }
+
+
+
+    -- Cmp
+
+    use { 'hrsh7th/nvim-cmp',
+          event = { 'InsertEnter', 'CursorHold', 'CursorMoved' },
+          config = [[ require 'configs/cmp/cmp' ]] }
+
+    use { 'hrsh7th/cmp-buffer',
+          module = 'cmp_buffer',
+          after = 'nvim-cmp' }
+
+    use { 'hrsh7th/cmp-path',
+          module = 'cmp_path',
+          after = 'nvim-cmp' }
+
+    use { 'hrsh7th/cmp-nvim-lua',
+          after = 'nvim-cmp' }
+
+    use { 'hrsh7th/cmp-nvim-lsp',
+          module = 'cmp_nvim_lsp',
+          after = 'nvim-cmp' }
+
+    use { 'neovim/nvim-lspconfig',
+          module = 'lspconfig',
+          after = 'nvim-cmp' }
+
+    use { 'saadparwaiz1/cmp_luasnip',
+          after = 'nvim-cmp' }
+
+
+
+    -- Treesitter
+
+    use { 'nvim-treesitter/nvim-treesitter',
+          run = ':TSUpdate',
+          config = [[ require 'configs/treesitter' ]] }
+
+    use { 'p00f/nvim-ts-rainbow' }
+
+    use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+
+
+
+    -- Misc things
+
+    use { 'nvim-lua/plenary.nvim',
+          module = 'plenary' }
+
+
+
+    -- Packer itself
+
+    use 'wbthomason/packer.nvim'
 
 end )
 
 
 vim.cmd [[
-  command! P :PackerSync
+    command! P :PackerSync
 ]]
 
