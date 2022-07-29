6195c54608188a9c7333dbbef7b2ea2b3f1b5861
MidAutumnMoon <me@418.im>
2022-07-29 11:36:34 +0800

    configs: matchup: tweak

--- lua/configs/matchup.lua
+++ lua/configs/matchup.lua
@@ -1,4 +1,12 @@
 vim.g.matchup_surround_enabled = 1
 vim.g.matchup_transmute_enabled = 1
+
 vim.g.matchup_matchparen_deferred = 1
 vim.g.matchup_matchparen_deferred_show_delay = 30
+
+vim.g.matchup_matchparen_timeout = 200
+vim.g.matchup_matchparen_insert_timeout = 50
+
+vim.g.matchup_matchparen_offscreen = {
+    method = 'popup'
+}
