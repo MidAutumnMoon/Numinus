2e058ca8e5886c42d85ee2a1c43e83b126c4263b
MidAutumnMoon <me@418.im>
2024-10-22 13:09:48 +0800

    plugin/lualine: reformat

--- lua/plugin/lualine/components.lua
+++ lua/plugin/lualine/components.lua
@@ -1,28 +1,26 @@
 local M = {}
 
-
 M.filename = {
-  "filename",
-  path = 1,
-  newfile_status = true,
-  symbols = {
-    modified = "[+]",
-    readonly = "[RO]",
-    unnamed = "[No Name]",
-    newfile = "[New]",
-  },
-  padding = {
-    left = 0,
-    right = 1
-  }
+    "filename",
+    path = 1,
+    newfile_status = true,
+    symbols = {
+        modified = "[+]",
+        readonly = "[RO]",
+        unnamed = "[No Name]",
+        newfile = "[New]",
+    },
+    padding = {
+        left = 0,
+        right = 1
+    }
 }
 
 M.progress_thru_buffer = {
-  "%P",
-  padding = {
-    right = 0
-  }
+    "%P",
+    padding = {
+        right = 0
+    }
 }
-
 
 return M
--- lua/plugin/lualine/init.lua
+++ lua/plugin/lualine/init.lua
@@ -8,49 +8,61 @@ local sources = require "plugin.lualine.sources"
 
 
 local sections = {
-  lualine_a = { filename },
-  lualine_b = { },
-  lualine_c = {
-    { "diff", source = sources.gitsigns },
-    { "diagnostic-message" }
-  },
-  lualine_x = {
-    { "encoding",
-      cond = function()
-        return vim.o.encoding ~= "utf-8"
-      end
+    lualine_a = { filename },
+    lualine_b = { },
+    lualine_c = {
+        { "diff", source = sources.gitsigns },
+        {
+            "diagnostic-message",
+            icons = {
+                error = "E:",
+                warn = "W:",
+                info = "I:",
+                hint = "H:",
+            };
+        }
     },
-    { "fileformat",
-      cond = function()
-        return vim.bo.fileformat ~= "unix"
-      end
+    lualine_x = {
+        {
+            "encoding",
+            cond = function()
+                return vim.o.encoding ~= "utf-8"
+            end
+        },
+        {
+            "fileformat",
+            cond = function()
+                return vim.bo.fileformat ~= "unix"
+            end
+        },
+        "filetype"
     },
-    "filetype"
-  },
-  lualine_y = { "location" },
-  lualine_z = { progress_thru_buffer }
+    lualine_y = { "location" },
+    lualine_z = { progress_thru_buffer }
 }
 
 
 local inactive_sections = {
-  lualine_a = { },
-  lualine_b = { },
-  lualine_c = { filename },
-  lualine_x = {
-    { "encoding",
-      cond = function()
-        return vim.o.encoding ~= "utf-8"
-      end
+    lualine_a = { },
+    lualine_b = { },
+    lualine_c = { filename },
+    lualine_x = {
+        {
+            "encoding",
+            cond = function()
+                return vim.o.encoding ~= "utf-8"
+            end
+        },
+        {
+            "fileformat",
+            cond = function()
+                return vim.bo.fileformat ~= "unix"
+            end
+        },
+        "filetype"
     },
-    { "fileformat",
-      cond = function()
-        return vim.bo.fileformat ~= "unix"
-      end
-    },
-    "filetype"
-  },
-  lualine_y = { "location" },
-  lualine_z = { progress_thru_buffer }
+    lualine_y = { "location" },
+    lualine_z = { progress_thru_buffer }
 }
 
 
@@ -58,22 +70,22 @@ local NONE = { bg = "NONE" }
 
 lualine.setup {
 
-  options = {
+    options = {
 
-    icons_enabled = true,
-    component_separators = "",
-    section_separators = "",
-    refresh = { statusline = 200 },
+        icons_enabled = true,
+        component_separators = "",
+        section_separators = "",
+        refresh = { statusline = 200 },
 
-    theme = {
-      normal = {
-        a = NONE, b = NONE, c = NONE,
-        x = NONE, y = NONE, z = NONE,
-      },
+        theme = {
+            normal = {
+                a = NONE, b = NONE, c = NONE,
+                x = NONE, y = NONE, z = NONE,
+            },
+        },
+
     },
 
-  },
-
-  sections = sections,
-  inactive_sections = inactive_sections
+    sections = sections,
+    inactive_sections = inactive_sections
 }
